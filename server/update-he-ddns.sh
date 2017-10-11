8c70cf3ae954c31163450fe0f42ba291b1deec77
CUI Hao (Lab) <cuihao.leo@gmail.com>
2017-10-11 20:28:28 +0800

    update-he-ddns.sh

---
+++ server/update-he-ddns.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DOMAIN=$1
+KEY=$2
+
+IPV4=$(ip route get 1 | grep -Po '(?<=src )[^ ]+')
+IPV6=$(ip route get 2001:: | grep -Po '(?<=src )[^ ]+')
+
+if [[ -n $IPv4 ]]; then 
+    echo "Update IPv4: $IPV4"
+    curl -q "https://dyn.dns.he.net/nic/update" \
+        -d "hostname=$DOMAIN" \
+        -d "password=$KEY" \
+        -d "myip=$IPV4" \
+        --connect-timeout 2000
+    echo
+fi
+
+if [[ -n $IPv6 ]]; then 
+    echo "Update IPv6: $IPV6"
+    curl -q "https://dyn.dns.he.net/nic/update" \
+        -d "hostname=$DOMAIN" \
+        -d "password=$KEY" \
+        -d "myip=$IPV6" \
+        --connect-timeout 2000
+fi
